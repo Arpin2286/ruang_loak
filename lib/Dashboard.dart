7bde0941442c066e2f9cc9c54231b4404372d759
Arpin2286 <68496697+Arpin2286@users.noreply.github.com>
2021-04-11 21:01:34 +0800

    no message

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -17,142 +17,92 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.lightGreen[100],
-        appBar: new AppBar(
-          title: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "RUANG LOAK",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ]),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(
-                  Icons.notifications_none,
-                  size: 30,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Notifikasi();
-                  }));
-                }),
+      backgroundColor: Colors.lightGreen[100],
+      appBar: new AppBar(
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Text(
+            "RUANG LOAK",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ]),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.notifications_none,
+                size: 30,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Notifikasi();
+                }));
+              }),
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text(
+                "Anggie Arpin",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              accountEmail: new Text("anggie.arpin.1@gmail.com"),
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: new NetworkImage(
+                    "https://media-exp1.licdn.com/dms/image/C5603AQHbZKgzo1QAyg/profile-displayphoto-shrink_200_200/0/1612793112343?e=1619049600&v=beta&t=lF-xB5VBzKwcjh9BH_Efp7Uk59YOd95Bczh7ekTPsrw"),
+              ),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover)),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.person),
+              title: new Text("Account"),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Profil.Profile(
+                    nama: "I Gede Anggie Suardika Arpin",
+                    pekerjaan: "Mahasiswa",
+                    image:
+                        "https://media-exp1.licdn.com/dms/image/C5603AQHbZKgzo1QAyg/profile-displayphoto-shrink_200_200/0/1612793112343?e=1619049600&v=beta&t=lF-xB5VBzKwcjh9BH_Efp7Uk59YOd95Bczh7ekTPsrw",
+                    lokasi: "Desa Kubutambahan",
+                  );
+                }));
+              },
+            ),
+            new ListTile(
+              leading: new Icon(Icons.email_outlined),
+              title: new Text("Message"),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Message();
+                }));
+              },
+            ),
+            new ListTile(
+              leading: new Icon(Icons.add_shopping_cart_sharp),
+              title: new Text("Jual Barang"),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Home();
+                }));
+              },
+            ),
+            new ListTile(
+              leading: new Icon(Icons.info_outline),
+              title: new Text("About"),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return about.About();
+                }));
+              },
+            ),
           ],
         ),
-        drawer: new Drawer(
-          child: new ListView(
-            children: <Widget>[
-              new UserAccountsDrawerHeader(
-                accountName: new Text(
-                  "Anggie Arpin",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                accountEmail: new Text("anggie.arpin.1@gmail.com"),
-                currentAccountPicture: new CircleAvatar(
-                  backgroundImage: new NetworkImage(
-                      "https://media-exp1.licdn.com/dms/image/C5603AQHbZKgzo1QAyg/profile-displayphoto-shrink_200_200/0/1612793112343?e=1619049600&v=beta&t=lF-xB5VBzKwcjh9BH_Efp7Uk59YOd95Bczh7ekTPsrw"),
-                ),
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover)),
-              ),
-              new ListTile(
-                leading: new Icon(Icons.person),
-                title: new Text("Account"),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Profil.Profile(
-                      nama: "I Gede Anggie Suardika Arpin",
-                      pekerjaan: "Mahasiswa",
-                      image:
-                          "https://media-exp1.licdn.com/dms/image/C5603AQHbZKgzo1QAyg/profile-displayphoto-shrink_200_200/0/1612793112343?e=1619049600&v=beta&t=lF-xB5VBzKwcjh9BH_Efp7Uk59YOd95Bczh7ekTPsrw",
-                      lokasi: "Desa Kubutambahan",
-                    );
-                  }));
-                },
-              ),
-              new ListTile(
-                leading: new Icon(Icons.email_outlined),
-                title: new Text("Message"),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Message();
-                  }));
-                },
-              ),
-              new ListTile(
-                leading: new Icon(Icons.info_outline),
-                title: new Text("About"),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return about.About();
-                  }));
-                },
-              ),
-            ],
-          ),
-        ),
-        body: GridView.count(
-            crossAxisCount: 2,
-            padding: EdgeInsets.all(40),
-            crossAxisSpacing: 25,
-            mainAxisSpacing: 40,
-            children: <Widget>[
-              new GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext content) =>
-                          SemuaDaftar.SemuaDaftar()));
-                },
-                child: Button(
-                  image: "assets/1.jpg",
-                  nama: "Shopping",
-                ),
-              ),
-              new GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext content) => Promo.Promo()));
-                },
-                child: Button(
-                  image: "assets/2.jpg",
-                  nama: "Discount",
-                ),
-              ),
-              new GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext content) =>
-                          PenggunaLain.PenggunaLain()));
-                },
-                child: Button(
-                  image: "assets/3.jpg",
-                  nama: "Other User",
-                ),
-              ),
-              new GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext content) => Home()));
-                },
-                child: Button(
-                  image: "assets/5.jpg",
-                  nama: "Sell Now",
-                ),
-              ),
-              new GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext content) => about.About()));
-                },
-                child: Button(
-                  image: "assets/4.jpg",
-                  nama: "About",
-                ),
-              )
-            ]));
+      ),
+    );
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'Dashboard.dart' as Dashboard;
+import 'Dashboard.dart';
+import 'Promo.dart';
+import 'pengguna_lain.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -7,5 +9,45 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: new Dashboard.Dashboard()));
+      home: NavBot()));
+}
+
+class NavBot extends StatefulWidget {
+  @override
+  _NavBotState createState() => _NavBotState();
+}
+
+class _NavBotState extends State<NavBot> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetOption = <Widget>[
+    Dashboard(),
+    Promo(),
+    PenggunaLain(),
+  ];
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOption.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.assignment), title: Text("Promo")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_add), title: Text("Tambah Teman"))
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+      ),
+    );
+  }
 }
